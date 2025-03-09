d4df14e072ddc391818184277f1ad8a19649e4b9
Tourlat <foucaultfelix33@gmail.com>
2025-03-09 21:33:41 +0100

    Smoother transitions

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const typingEffectProjects = document.getElementById('typing-effect-projects');
     const textProjects = "~/Projects";
-    const projectsContent = document.getElementById('projects-content');
     typingEffectProjects.textContent = '';
     const enterCommand = document.getElementById('enter-command');
 
     function showProjectsContent() {
         enterCommand.style.display = 'none';
-        projectsContent.style.display = 'block';
-        projectsContent.classList.add('show');
+        document.querySelector('.info').classList.add('show');
     }
 
     enterCommand.textContent = "Enter command_";
